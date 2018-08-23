9807c65c57c0e5c030892a3f6ecd5b386debcb39
Ankit <ankit.b09@gmail.com>
2018-08-23 23:44:01 +0530

    search form

--- flight_search/src/containers/search.js
+++ flight_search/src/containers/search.js
@@ -4,19 +4,27 @@ import React, { Component } from 'react';
 class Search extends Component {
     constructor(props) {
         super(props);
-        this.setState({
-            form: {
-                originCity: ''
-            }
-        })
-    }
-    
-    formValues(e) {
+        this.state = {
+                originCity: '',
+                destCity:'',
+                depDate: '',
+                returnDate: '',
+                passengerNo:''
 
+        };
+
+        this.formValues = this.formValues.bind(this);
+    }
+
+    formValues(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
 
     render() {
+        const {originCity,destCity,depDate,returnDate,passengerNo} = this.state;
         return (
             <div className="container">
                 <div className="row">
@@ -24,23 +32,23 @@ class Search extends Component {
                         <form name="">
                             <div className="form-group">
                                 <label htmlFor="originCity">Enter Origin City</label>
-                                <input className="form-control" id="originCity" type="text" onChange={this.formValues} value={this.state.form.originCity} />
+                                <input name="originCity" className="form-control" id="originCity" type="text" onChange={this.formValues} value={originCity} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="destCity">Enter Destination City</label>
-                                <input className="form-control" id="destCity" type="text" onChange={this.formValues} value={this.state.form.originCity} />
+                                <input name="destCity" className="form-control" id="destCity" type="text" onChange={this.formValues} value={destCity} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="depDate">Enter Departure Date</label>
-                                <input className="form-control" id="depDate" placeholder="hi" type="date" />
+                                <input name="depDate" className="form-control" id="depDate" placeholder="hi" type="date" onChange={this.formValues} value={depDate} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="returnDate">Enter Return Date</label>
-                                <input className="form-control" id="returnDate" placeholder="hi" type="date" />
+                                <input name="returnDate" className="form-control" id="returnDate" placeholder="hi" type="date" onChange={this.formValues} value={returnDate} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="passengerNo">Enter No of Passenger</label>
-                                <input className="form-control" id="passengerNo" type="text" defaultValue="" />
+                                <input name="passengerNo" className="form-control" id="passengerNo" type="text" onChange={this.formValues} value={passengerNo} />
                             </div>
                             <button type="submit" className="btn btn-primary">Submit</button>
                         </form>
