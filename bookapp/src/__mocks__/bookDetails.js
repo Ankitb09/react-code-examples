9159327b4350577ed8dce8cf86abbe7d4e0af4af
Ankit <ankit.b09@gmail.com>
2019-06-04 07:08:26 +0530

    Adding component test cases

--- bookapp/src/__mocks__/bookDetails.js
+++
@@ -1,9 +0,0 @@
-/*
-  Dummy data for testing async actions and other unit test.
-  Currently it is exact copy of Books data.
-*/
-
-let bookDetails ='{"image_url":"https://images.blinkist.com/images/books/5c49e2896cee070007b962ac/3_4/640.jpg","id":"5c5ae9fc608b273a5257042c","category_id":"5c5ae8a3608b273a52570420","title":"Cupressus est vos cubicularis","content":"Eum reprehenderit natus sit alias nostrum sit. Vel eveniet quidem rerum voluptatibus. Aut provident expedita iste repudiandae tenetur. Dolore laudantium repudiandae quia qui voluptatibus. Quis sapiente perferendis similique non exercitationem voluptatem. Quasi et beatae animi exercitationem officiis cum. Enim tempora et harum impedit. Nulla quisquam quia. Qui est ipsum voluptas necessitatibus non. Corrupti dolores ut tempora. Voluptatem perspiciatis et. Voluptates fugit tenetur. Et blanditiis magni id id nemo est. Fugiat soluta et aperiam aliquam et nihil. Libero soluta reiciendis quo sit est et nulla. Alias accusantium modi. Ut est ut fugiat nam. Laboriosam soluta fuga. Dolorum perferendis beatae. Qui autem recusandae omnis."}';
-
-
-export default JSON.parse(bookDetails);
